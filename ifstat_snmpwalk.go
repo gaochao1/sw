9f8c7cb5774491eb0bd0499da83e38be05618a32
gaochao <gaogch@gmail.com>
2015-07-09 15:47:21 +0800

    bug fix

--- ifstat_snmpwalk.go
+++ ifstat_snmpwalk.go
@@ -75,10 +75,11 @@ func ListIfStatsSnmpWalk(ip, community string, timeout int, ignoreIface []string
 				}
 
 				ifStats.TS = now
+
+				ifName = strings.Replace(ifname, `"`, "", -1)
 				ifStats.IfName = ifName
 
 				ifStatsList = append(ifStatsList, ifStats)
-
 			}
 		}
 	}
