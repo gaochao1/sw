66e30019b92f79801a441f1d0b22842cabe5dd36
gaochao <gaogch@gmail.com>
2015-07-13 11:39:39 +0800

    bug fix

--- modelstat.go
+++ modelstat.go
@@ -1,6 +1,7 @@
 package sw
 
 import (
+	"log"
 	"regexp"
 )
 
@@ -9,13 +10,19 @@ func SysModel(ip, community string, timeout int) (string, error) {
 	method := "get"
 	var oid string
 
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Recovered in sw.modelstat.go SysModel", r)
+		}
+	}()
+
 	switch vendor {
 	case "Cisco_NX":
 		oid = "1.3.6.1.2.1.47.1.1.1.1.13.10"
 	case "Cisco":
 		oid = "1.3.6.1.2.1.47.1.1.1.1.13.1001"
 	case "Huawei", "H3C", "H3C_V5", "H3C_V7":
-		re := regexp.MustCompile(`\w+\s+\w+-\w+-\w+`)
+		re := regexp.MustCompile(`\w+-\w+-\w+\S+`)
 		sysDescr, err := SysDescr(ip, community, timeout)
 		if err != nil {
 			return "", err
